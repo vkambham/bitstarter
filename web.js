cd4ed1186fdd972e4813c8f5efd00236d9821a10
Venkat Kambhammettu <vkambham@gmail.com>
2013-07-10 15:50:16 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
+var buf = new Buffer(256);
+
+buf= fs.readFileSync('index.html',function(err,data){
+   if(err) throw err;
+   console.log(data);
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
